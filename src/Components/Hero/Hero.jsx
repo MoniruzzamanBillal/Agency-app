f9a947259693f21c27d0cb4d87cc771835d7c00b
Billal <mdmoniruzzamanbillal@gmail.com>
2023-04-23 21:59:25 +0600

    hero section done

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Hero.css";
+import heroImg from "../../images/hero-img.png";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="container">
+        <div className="heroWrapper">
+          <div className="heroLeft">
+            <div className="heroHeadings">
+              <h1>We're creating perfect</h1>
+              <h1>Digital product to</h1>
+              <h1 className="heroBranding">Promote your brand</h1>
+            </div>
+
+            <p className="description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+              ipsum dicta veniam corporis blanditiis eaque aut quos possimus
+              assumenda eveniet.
+            </p>
+
+            <div className="heroButton">
+              <div className="btn1">
+                <button className="btn btn1">Get started now</button>
+              </div>
+
+              <div className="btn2">
+                <button className="btn btn2">Discover now</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="heroRight">
+            <div className="heroImg">
+              <img src={heroImg} alt="Hero image" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -29,48 +29,50 @@ export default function NavBar() {
 
   return (
     <div className="container">
-      <div className="NavWrapper">
-        {/* left side  */}
-        <div className="logo left">
-          <h1>Logo</h1>
-        </div>
-        {/* left side ends  */}
-        {/* middle side  */}
-        <div className="NavLinks middle">
-          <ul className={menuActive ? "Menu MobileMenuLink" : "Menu"}>
-            {NavLinks.map((ele, index) => (
-              <li className="Menu_items" key={index}>
-                <a
-                  href={ele.path}
-                  className="Menu_links"
-                  onClick={() =>
-                    menuActive
-                      ? setMenuActive(!menuActive)
-                      : setMenuActive(menuActive)
-                  }
-                >
-                  {ele.display}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        {/* middle side ends  */}
-        {/* right side  */}
-        <div className=" right">
-          <div className="light-theme">
-            <i className="ri-sun-line"></i>
-            <span>Light Mode</span>
+      <div className="navContainer">
+        <div className="NavWrapper">
+          {/* navLeft side  */}
+          <div className="logo navLeft">
+            <h1>Logo</h1>
           </div>
+          {/* navLeft side ends  */}
+          {/* middle side  */}
+          <div className="NavLinks middle">
+            <ul className={menuActive ? "Menu MobileMenuLink" : "Menu"}>
+              {NavLinks.map((ele, index) => (
+                <li className="Menu_items" key={index}>
+                  <a
+                    href={ele.path}
+                    className="Menu_links"
+                    onClick={() =>
+                      menuActive
+                        ? setMenuActive(!menuActive)
+                        : setMenuActive(menuActive)
+                    }
+                  >
+                    {ele.display}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* middle side ends  */}
+          {/* navRight side  */}
+          <div className=" navRight">
+            <div className="light-theme">
+              <i className="ri-sun-line"></i>
+              <span>Light Mode</span>
+            </div>
 
-          <div className="HamBurgerMenu ">
-            <i
-              className={menuActive ? " ri-close-line " : "ri-menu-line"}
-              onClick={() => setMenuActive(!menuActive)}
-            ></i>
+            <div className="HamBurgerMenu ">
+              <i
+                className={menuActive ? " ri-close-line " : "ri-menu-line"}
+                onClick={() => setMenuActive(!menuActive)}
+              ></i>
+            </div>
           </div>
+          {/* navRight side ends  */}
         </div>
-        {/* right side ends  */}
       </div>
     </div>
   );
