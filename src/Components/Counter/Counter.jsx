d49d222885a7804f255fc0a6356a09e36b9e17fc
Billal <mdmoniruzzamanbillal@gmail.com>
2023-04-25 23:12:19 +0600

    mode functionality done

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -19,15 +19,17 @@ const CounterData = [
 export default function Counter() {
   return (
     <>
-      <div className="container">
-        <div className="counter">
-          <div className="counterWrapper">
-            {CounterData.map((ele, index) => (
-              <div className="counterItem" key={index}>
-                <h3 className="counterNumber">{ele.number}+</h3>
-                <h4 className="counterTitle">{ele.text}</h4>
-              </div>
-            ))}
+      <div className="CounterContainer">
+        <div className="container">
+          <div className="counter">
+            <div className="counterWrapper">
+              {CounterData.map((ele, index) => (
+                <div className="counterItem" key={index}>
+                  <h3 className="counterNumber">{ele.number}+</h3>
+                  <h4 className="counterTitle">{ele.text}</h4>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -30,55 +30,68 @@ export default function NavBar({ theme, ToggleTheme }) {
   const [menuActive, setMenuActive] = useState(false);
 
   return (
-    <div className="container ">
-      <div className="navContainer ">
-        <div className="NavWrapper">
-          {/* navLeft side  */}
-          <div className="logo navLeft">
-            {/* <h1>Logo</h1> */}
-            <h1>
-              <a href="#Home">Logo</a>
-            </h1>
-          </div>
-          {/* navLeft side ends  */}
-          {/* middle side  */}
-          <div className="NavLinks middle">
-            <ul className={menuActive ? "Menu MobileMenuLink" : "Menu"}>
-              {NavLinks.map((ele, index) => (
-                <li className="Menu_items" key={index}>
-                  <a
-                    href={ele.path}
-                    className="Menu_links"
-                    onClick={() =>
-                      menuActive
-                        ? setMenuActive(!menuActive)
-                        : setMenuActive(menuActive)
-                    }
-                  >
-                    {ele.display}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          {/* middle side ends  */}
-          {/* navRight side  */}
-          <div className=" navRight">
-            <div className="light-theme" onClick={() => ToggleTheme()}>
-              <i className="ri-sun-line"></i>
-              <span>Light Mode</span>
-            </div>
+    <>
+      <div className="NavParentContainer">
+        <div className="container ">
+          <div className="navContainer ">
+            <div className="NavWrapper">
+              {/* navLeft side  */}
+              <div className="logo navLeft">
+                {/* <h1>Logo</h1> */}
+                <h1>
+                  <a href="#Home">Logo</a>
+                </h1>
+              </div>
+              {/* navLeft side ends  */}
+              {/* middle side  */}
+              <div className="NavLinks middle">
+                <ul className={menuActive ? "Menu MobileMenuLink" : "Menu"}>
+                  {NavLinks.map((ele, index) => (
+                    <li className="Menu_items" key={index}>
+                      <a
+                        href={ele.path}
+                        className="Menu_links"
+                        onClick={() =>
+                          menuActive
+                            ? setMenuActive(!menuActive)
+                            : setMenuActive(menuActive)
+                        }
+                      >
+                        {ele.display}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              {/* middle side ends  */}
+              {/* navRight side  */}
+              <div className=" navRight">
+                <div className="changeTheme" onClick={() => ToggleTheme()}>
+                  {theme === "light-theme" ? (
+                    <>
+                      <i className="ri-moon-fill"></i>
+                      <span>Dark Mode</span>
+                    </>
+                  ) : (
+                    <>
+                      <i className="ri-sun-line"></i>
+                      <span>Light Mode</span>
+                    </>
+                  )}
+                </div>
 
-            <div className="HamBurgerMenu ">
-              <i
-                className={menuActive ? " ri-close-line " : "ri-menu-line"}
-                onClick={() => setMenuActive(!menuActive)}
-              ></i>
+                <div className="HamBurgerMenu ">
+                  <i
+                    className={menuActive ? " ri-close-line " : "ri-menu-line"}
+                    onClick={() => setMenuActive(!menuActive)}
+                  ></i>
+                </div>
+              </div>
+              {/* navRight side ends  */}
             </div>
           </div>
-          {/* navRight side ends  */}
         </div>
       </div>
-    </div>
+    </>
   );
 }
